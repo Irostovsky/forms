26dbe66425193071ad738e3736020db0a8a94661
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2024-09-25 10:35:14 +0300

    Get form inputs values by input state

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,39 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(`Submit: email ${enteredEmail}, password: ${enteredPassword}`);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(e) => setEnteredEmail(e.target.value)}
+            value={enteredEmail}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(e) => setEnteredPassword(e.target.value)}
+            value={enteredPassword}
+          />
         </div>
       </div>
 
